df6cdc954006aef056519f971ca3c352c7ee08d7
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-04-24 09:14:29 +0700

    added add stok feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -13,6 +13,35 @@ class AdminController{
             res.render('error', {error})
         })
     }
+
+    static getAdd(req, res){
+        Item.findAll({
+            order: [['id', 'asc']]
+        })
+        .then(data => {
+            res.render('admin_add_item', {data})
+        })
+        .catch(error => {
+            res.render('error', {error})
+        })
+    }
+
+    static postAdd(req, res){
+        console.log(req.body)
+        Item.update({
+            quantity: Number(req.body.quantity[0]) + Number(req.body.quantity[1])
+        }, {
+            where: {
+                id: Number(req.body.id)
+            }
+        })
+        .then(() => {
+            res.redirect('/login/admin/add')
+        })
+        .catch(error => {
+            res.render('error', {error})
+        })
+    }
 }
 
 module.exports = AdminController
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const Controller = require('../controllers/adminController')
 
 router.get('/', Controller.datas)
+router.get('/add', Controller.getAdd)
+router.post('/add', Controller.postAdd)
 
 module.exports = router
